a8b82a3dcf0406ced450fc4cf13952c1dbba7eee
Amedeo Bertuzzi <giangiallo99@gmail.com>
2023-10-06 08:36:07 +0000

    Added proxy tree

--- decomposed_main.py
+++ decomposed_main.py
@@ -44,6 +44,8 @@ import json
 
 """--------------------------------------------------"""
 
+proxytree = fn.Proxytree()
+
 DEBUG = True            # Debug boolean
 SAVE_DICT = False
 LOAD_DICT = False
@@ -80,7 +82,7 @@ else:
     LINKS += 2*(2**(DEPTH-1))
 
 server_capacity = [SERVER_C for i in range(SERVERS)]           # Capacity of each server
-
+    
 link_capacity = []
 # Switch links
 for lvl in range(DEPTH-1):
@@ -112,6 +114,16 @@ if DEBUG:
     print("DYNAMIC Power Consumption: ", *[s for s in dyn_powcons])
     print("VM's CPU Utilization: ", *[s for s in cpu_util])
     print("Flow Path Data Rate: ", *[s for s in data_rate])
+    print("\n\n")
+
+    print("SWITCH Indexes: ", *[k for k in range(proxytree.SWITCHES)])
+    print("SERVER Indexes: ", *[s+proxytree.SWITCHES for s in range(proxytree.SERVERS)])
+    print("SERVER Capacity: ", *[s for s in proxytree.server_capacity])
+    print("LINK Capacity: ", *[s for s in proxytree.link_capacity])
+    print("IDLE Power Consumption: ", *[s for s in proxytree.idle_powcons])
+    print("DYNAMIC Power Consumption: ", *[s for s in proxytree.dyn_powcons])
+    print("VM's CPU Utilization: ", *[s for s in proxytree.cpu_util])
+    print("Flow Path Data Rate: ", *[s for s in proxytree.data_rate])
     print("\n\n")
 
 
@@ -177,6 +189,14 @@ if DEBUG:
                 print(j, " (link ", link_dict.get(str((i,j))) ,")", sep="", end="\t")
     print("\n\n")
 
+    print("### Tree Structure ###")
+    for i in range(len(proxytree.adjancy_list)):
+        print("\nNodo ", i, " collegato ai nodi:", end="\t")
+        for j in range(len(proxytree.adjancy_list)):
+            if proxytree.adjancy_list[i][j] == 1:
+                print(j, " (link ", proxytree.link_dict.get(str((i,j))) ,")", sep="", end="\t")
+    print("\n\n")
+
 
 # Creating communicating VMS
 src_dst = [[0 for j in range(2)] for i in range(FLOWS)]
@@ -192,7 +212,13 @@ if DEBUG:
         print("Path ", src_dst.index(path), ": ", end="\t")
         print( *[s for s in path], sep="  -  ")
     print("\n")
+    print("### VM Paths ###")
+    for path in proxytree.src_dst:
+        print("Path ", proxytree.src_dst.index(path), ": ", end="\t")
+        print( *[s for s in path], sep="  -  ")
+    print("\n")
 
+exit()
 
 ############### VM MODEL BINARY VARIABLES ###################################
 server_status = [dimod.Binary("s" + str(i)) for i in range(SERVERS)]          # Binary value for each server, 1 ON, 0 OFF
--- fun_lib.py
+++ fun_lib.py
@@ -1,3 +1,6 @@
+import numpy as np
+import random
+
 def get_nodes(l, dictionary):
     '''
     A function that returns a tuple (n1,n2) containing
@@ -10,3 +13,112 @@ def get_nodes(l, dictionary):
     nodes = nodes.replace("(", "")
     nodes = nodes.replace(")", "")
     return tuple(map(int, nodes.split(', ')))
+
+class Proxytree():
+    def __init__(self):
+        self.DEPTH = 3
+
+        self.SERVER_C = 10               # Server capacity
+        self.LINK_C = 5*self.DEPTH            # Link capacity
+        self.LINK_C_DECREASE = 2
+        self.IDLE_PC = 10*self.DEPTH          # Idle power consumption
+        self.IDLE_PC_DECREASE = 5        # Idle power consumption
+        self.DYN_PC = 2*self.DEPTH            # Dynamic power consumption
+        self.DYN_PC_DECREASE = 1         # Dynamic power consumption
+        self.REQ_AVG = 8                 # Average flow request
+        self.DATAR_AVG = 4               # Average data rate per flow
+        self.LAGRANGE_MUL = self.IDLE_PC*10   # Lagrange multiplier for cqm -> bqm conversion
+
+        self.SERVERS = pow(2, self.DEPTH)                                 # Server number
+        self.SWITCHES = sum(pow(2,i) for i in range(self.DEPTH))          # Switch number
+        self.VMS = self.SERVERS                                           # VM number per server
+        self.FLOWS = self.VMS//2 if self.VMS%2==0 else self.VMS//2+1                # Flow number
+        self.NODES = self.SERVERS + self.SWITCHES                              # Total Nodes
+        self.LINKS = 0
+        self.init_links()
+
+        self.server_capacity = [self.SERVER_C for i in range(self.SERVERS)]           # Capacity of each server
+        self.link_capacity = []
+        self.idle_powcons = []           # Idle power consumption of each node
+        self.dyn_powcons = []            # Dynamic power of each node
+        self.init_link_capacity()
+        self.init_idle_dyn_powcons()
+        self.cpu_util = (np.random.normal(self.REQ_AVG, 1, self.VMS)).astype(int)         # CPU utilization of each VM
+        self.data_rate = (np.random.normal(self.DATAR_AVG, 1, self.FLOWS)).astype(int)      # Data rate of flow f on link l 
+        
+        self.link_dict = {}
+        self.adjancy_list = [[0 for j in range(self.NODES)] for i in range(self.NODES)] 
+        self.init_link_dict_adj_list()
+
+        self.src_dst = [[0 for j in range(2)] for i in range(self.FLOWS)]
+        self.initi_src_dst()
+
+
+    def init_links(self):
+        for i in range(self.DEPTH-1):
+            self.LINKS += 2**i * 2**(i+1)
+        self.LINKS += 2*(2**(self.DEPTH-1))
+    
+    def init_link_capacity(self):
+        # Switch links
+        for lvl in range(self.DEPTH-1):
+            for _ in range(2**(2*lvl+1)):
+                self.link_capacity.append(self.LINK_C)
+            self.LINK_C -= self.LINK_C_DECREASE
+        # Server links
+        for _ in range(2**self.DEPTH):
+            self.link_capacity.append(self.LINK_C)
+        
+    def init_idle_dyn_powcons(self):
+        for lvl in range(self.DEPTH+1):
+            for _ in range(2**lvl):
+                self.idle_powcons.append(self.IDLE_PC)
+                self.dyn_powcons.append(self.DYN_PC)
+            self.IDLE_PC -= self.IDLE_PC_DECREASE
+            self.DYN_PC -= self.DYN_PC_DECREASE
+
+    def init_link_dict_adj_list(self):
+        link_counter = 0
+        # Create all sw-sw links
+        for lvl in range(self.DEPTH-1):
+            if lvl == 0:
+                for i in range(1,3):
+                    self.adjancy_list[0][i] = 1
+                    self.adjancy_list[i][0] = 1
+                    self.link_dict[str((0,i))] = link_counter
+                    self.link_dict[str((i,0))] = link_counter
+                    link_counter += 1
+            else:
+                first_sw = 2**(lvl) - 1
+                last_sw = first_sw * 2
+                for father in range(first_sw, last_sw + 1):
+                    first_son = 2**(lvl+1) - 1
+                    last_son = first_son * 2
+                    for son in range(first_son, last_son + 1):
+                        self.adjancy_list[father][son] = 1
+                        self.adjancy_list[son][father] = 1
+                        self.link_dict[str((father,son))] = link_counter
+                        self.link_dict[str((son,father))] = link_counter
+                        link_counter += 1
+        
+        # Last layer first and last switch
+        ll_firstsw = 2**(self.DEPTH-1) - 1
+        ll_lastsw = ll_firstsw * 2
+        
+        # Create all sw-s links
+        for father in range(ll_firstsw, ll_lastsw + 1):
+            for i in range(2):
+                son = father * 2 + 1 + i
+
+                self.adjancy_list[father][son] = 1
+                self.adjancy_list[son][father] = 1
+                self.link_dict[str((father,son))] = link_counter
+                self.link_dict[str((son,father))] = link_counter
+                link_counter += 1
+
+    def initi_src_dst(self):
+        index_list = [i for i in range(self.VMS)]
+        random.shuffle(index_list)
+        for i in range(self.FLOWS):
+            for j in range(2):
+                self.src_dst[i][j] = index_list[i*2 + j]
