bb928fd73cb4ffbad085c9bf26c24404432e8299
AmedeoB <137800476+AmedeoB@users.noreply.github.com>
2023-11-21 09:49:20 +0000

    dwave tests

--- main_DWAVE.py
+++ main_DWAVE.py
@@ -16,7 +16,7 @@ from fun_lib_DWAVE import *
 """--------------------------------------------------"""
 
 proxytree = Proxytree(
-                depth = 6, 
+                depth = 2, 
                 server_c = 10, 
                 link_c = 5, 
                 idle_pc = 10, 
@@ -35,7 +35,7 @@ manager = CQMmanager(
                 save_info_full = True
             )
 
-ITERATIONS = 4
+ITERATIONS = 5
 
 
 if DEBUG:   
@@ -46,44 +46,44 @@ if DEBUG:
 # ###################################################################
 # |                       VM MODEL                                  |
 # ###################################################################
-# print_section("VM Model")
+print_section("VM Model")
 
-# # Create problem
-# vm_cqm = dimod.ConstrainedQuadraticModel()
-# # Variables & Constraints
-# vm_model(proxytree, vm_cqm)
+# Create problem
+vm_cqm = dimod.ConstrainedQuadraticModel()
+# Variables & Constraints
+vm_model(proxytree, vm_cqm)
 
-# if DEBUG: print_model_structure("vm model", vm_cqm)
+if DEBUG: print_model_structure("vm model", vm_cqm)
 
-# # Solve
-# for _ in range(ITERATIONS):
-#     vm_cqm_solution, vm_cqm_info = detailed_cqm_solver(vm_cqm, "vm_model", 
-#                         proxytree.DEPTH, save_solution = manager.SAVE_VM_SOL,
-#                         save_info= manager.SAVE_VM_INFO)
+# Solve
+for _ in range(ITERATIONS):
+    vm_cqm_solution, vm_cqm_info = detailed_cqm_solver(vm_cqm, "vm_model", 
+                        proxytree.DEPTH, save_solution = manager.SAVE_VM_SOL,
+                        save_info= manager.SAVE_VM_INFO)
 
-#     if DEBUG:   print_cqm_extrainfo(vm_cqm_solution, vm_cqm_info)
+    if DEBUG:   print_cqm_extrainfo(vm_cqm_solution, vm_cqm_info)
 
 
 
-#     # ###################################################################
-#     # |                       PATH MODEL                                |
-#     # ###################################################################
-#     print_section("Path Model")
+    # ###################################################################
+    # |                       PATH MODEL                                |
+    # ###################################################################
+    print_section("Path Model")
 
-#     # Create problem
-#     path_cqm = dimod.ConstrainedQuadraticModel()
-#     # Variables & Constraints
-#     path_model(proxytree, path_cqm, vm_solution = vm_cqm_solution, 
-#             load = manager.LOAD_SOL)
+    # Create problem
+    path_cqm = dimod.ConstrainedQuadraticModel()
+    # Variables & Constraints
+    path_model(proxytree, path_cqm, vm_solution = vm_cqm_solution, 
+            load = manager.LOAD_SOL)
 
-#     if DEBUG: print_model_structure("path model", path_cqm)
+    if DEBUG: print_model_structure("path model", path_cqm)
 
-#     # Solve
-#     path_cqm_solution, path_cqm_info = detailed_cqm_solver(path_cqm, "path_model", 
-#                     proxytree.DEPTH, save_solution = manager.SAVE_PATH_SOL,
-#                     save_info= manager.SAVE_PATH_INFO)
+    # Solve
+    path_cqm_solution, path_cqm_info = detailed_cqm_solver(path_cqm, "path_model", 
+                    proxytree.DEPTH, save_solution = manager.SAVE_PATH_SOL,
+                    save_info= manager.SAVE_PATH_INFO)
 
-#     if DEBUG:   print_cqm_extrainfo(path_cqm_solution, path_cqm_info)
+    if DEBUG:   print_cqm_extrainfo(path_cqm_solution, path_cqm_info)
 
 
 
